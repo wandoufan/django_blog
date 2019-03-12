10da4562289c194dcf3dfa42f124c5246b671987
wandoufan <2416177709@qq.com>
2019-03-12 22:35:46 +0800

    修改url

--- my_blog/article/models.py
+++ my_blog/article/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
-from taggit.managers import TaggableManager
+# from taggit.managers import TaggableManager
 from PIL import Image
 
 
--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from article import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('article-create/', views.article_create, name='article_create'),
 ]
