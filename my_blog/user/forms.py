b1c21ae264f8c86aa54365ff70d3c5cdcb290f79
xingyifan <2416177709@qq.com>
2019-03-18 17:13:56 +0800

    增加注册功能

--- my_blog/user/forms.py
+++ my_blog/user/forms.py
@@ -1,13 +1,22 @@
 # 引入表单类
 from django import forms
+# 引入 User 模型
+from django.contrib.auth.models import User
 from .models import UserInfo
 
 
-# 用户的表单类
-class UserInfoForm(forms.Form):
+# 用户登录表单，继承forms.Form类
+class UserLoginForm(forms.Form):
     class Meta:
         # 指明数据模型来源
         model = UserInfo
         # 指明从html表单中返回的字段
         # fields = '__all__'
+        fields = ('username', 'password')
+
+
+# 用户注册表单，继承类
+class UserCreateForm(forms.ModelForm):
+    class Meta:
+        model = User
         fields = ('username', 'password')
--- my_blog/user/views.py
+++ my_blog/user/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import UserInfo
-from .forms import UserInfoForm
+from .forms import UserLoginForm
+from .forms import UserCreateForm
 from django.contrib.auth import authenticate, login, logout
 
 
@@ -9,7 +10,15 @@ def user_create(request):
     """
     创建新用户
     """
-    pass
+    if request.method == 'POST':
+        user_create_form = UserCreateForm(request.POST)
+        if user_create_form.is_valid():
+            user = user_create_form.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+            return HttpResponse('创建新用户成功')
+        else:
+            return HttpResponse('输入数据不合法：%s' % user_create_form.errors)
+    else:
+        return render(request, 'user/create.html')
 
 def user_login(request):
     """
@@ -17,12 +26,11 @@ def user_login(request):
     """
     if request.method == 'POST':
         # 将提交的数据赋值到表单实例中
-        user_info_form = UserInfoForm(request.POST)
-        if user_info_form.is_valid():
+        user_login_form = UserLoginForm(request.POST)
+        if user_login_form.is_valid():
             # cleaned_data方法得到一个包含表单数据的字典结构
-            # info = user_info_form.cleaned_data
+            # info = user_login_form.cleaned_data
             # 检查账户密码是否有效，并得到一个user对象
-            # user = authenticate(username=info['username'], password=info['password'])
             user = authenticate(username=request.POST['username'], password=request.POST['password'])
             if user is not None:
                 # authenticate方法的返回值不为None说明账户有效，可以登录
@@ -31,7 +39,7 @@ def user_login(request):
             else:
                 return HttpResponse('账户或密码有误')
         else:
-            return HttpResponse('输入数据不合法：%s' % user_info_form.errors)
+            return HttpResponse('输入数据不合法：%s' % user_login_form.errors)
     else:
         return render(request, 'user/login.html')
 
